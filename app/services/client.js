dca4ab74957e32dcb391a368defca4ddd5a7b5d1
Ayanesh Sarkar <ayabneshsarkar101@gmail.com>
2023-02-12 23:25:09 +0530

    Attach token on request headers :heavy_check_mark:

--- app/services/client.js
+++ app/services/client.js
@@ -1,8 +1,17 @@
 import { create } from "apisauce";
 import cache from '../utility/cache'
+import authStorage from '../auth/storage'
 
 const apiClient = create({
   baseURL: 'http://192.168.0.101:9000/api/'
+})
+
+apiClient.addAsyncRequestTransform(async (req) => {
+  const authToken = authStorage.getToken()
+
+  if (!authToken) return
+
+  req.headers['x-auth-token'] = authToken
 })
 
 const get = apiClient.get
